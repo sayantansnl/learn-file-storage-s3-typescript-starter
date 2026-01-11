ac4bf268dd99440795d8a0d06ea85ffb40567f59
sayantansnl <sayantansanyal72@gmail.com>
2026-01-11 05:59:03 +0000

    thumbnails updated

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -4,38 +4,6 @@ import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
 import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
-import { getInMemoryURL } from "./assets";
-
-type Thumbnail = {
-  data: ArrayBuffer;
-  mediaType: string;
-};
-
-const videoThumbnails: Map<string, Thumbnail> = new Map();
-
-export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
-  const { videoId } = req.params as { videoId?: string };
-  if (!videoId) {
-    throw new BadRequestError("Invalid video ID");
-  }
-
-  const video = getVideo(cfg.db, videoId);
-  if (!video) {
-    throw new NotFoundError("Couldn't find video");
-  }
-
-  const thumbnail = videoThumbnails.get(videoId);
-  if (!thumbnail) {
-    throw new NotFoundError("Thumbnail not found");
-  }
-
-  return new Response(thumbnail.data, {
-    headers: {
-      "Content-Type": thumbnail.mediaType,
-      "Cache-Control": "no-store",
-    },
-  });
-}
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -78,13 +46,11 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
     throw new Error("Error reading file data");
   }
 
-  videoThumbnails.set(videoId, {
-    data: fileData,
-    mediaType,
-  });
+  const buffer = Buffer.from(fileData);
+  const encodedData = buffer.toString("base64");
 
-  const urlPath = getInMemoryURL(cfg, videoId);
-  video.thumbnailURL = urlPath;
+  const dataURL = `data:${mediaType};base64,${encodedData}`;
+  video.thumbnailURL = dataURL;
   updateVideo(cfg.db, video);
 
   return respondWithJSON(200, video);
